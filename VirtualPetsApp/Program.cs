e911c10d4658641b518e8dd67acea970186ce283
William Harris <williamharris2021@gmail.com>
2019-09-09 14:30:44 -0400

    first commit

---
+++ VirtualPetsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirtualPetsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
